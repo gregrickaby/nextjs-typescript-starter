8ed3dad88fd0e09725be05180e1736d72e2dbbff
Greg Rickaby <gregrickaby@gmail.com>
2022-07-18 15:07:49 -0500

    experimental image support

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    images: {
+      allowFutureImage: true
+    }
+  },
+  images: {
+    formats: ['image/avif', 'image/webp']
+  },
   reactStrictMode: true,
   swcMinify: true
 }
